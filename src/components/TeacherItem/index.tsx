861407d7fb613e2c665263532e77ab3678268153
RnD <55111385+rnd18@users.noreply.github.com>
2020-08-06 16:53:10 -0300

    terceiro dia finalizado

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,30 +3,49 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem = () => {
+export interface Teacher {
+  id: number;
+  avatar:string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+ interface TeacherItemProps{
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection(){
+    api.post('connections',{
+      user_id: teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-      <img src="https://avatars0.githubusercontent.com/u/55111385?s=400&u=1b09bd0b12b7e38b56df162fae9bc415018fe4c9&v=4" alt="Ramon"/>
+      <img src={teacher.avatar} alt="Ramon"/>
         <div>
-            <strong>Ramon</strong>
-            <span>Informática</span>
+            <strong>{teacher.name}</strong>
+            <span>{teacher.subject}</span>
         </div>
         </header>
         <p>
-            Apaixonado por tecnologias avançadas.
-            <br/> <br/>
-            Procurando sempre compartilhar conhecimento aos meus alunos e mais de 2000 pessoas passaram por mim.
+            {teacher.bio}
         </p>
         <footer>
             <p>Preço/Hora
-            <strong>R$ 50,00</strong>
+              <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   )
